f839234c2ee65c5872af4f32f8ba797acb9bb6a5
kirillak <kirillak404@gmail.com>
2023-02-20 13:06:09 +0800

    Refactoring :)

---
+++ README.md
@@ -0,0 +1,19 @@
+# GPT-3 Telegram Chat-bot
+
+This is a fun chatbot built using OpenAI's GPT-3 and Telegram's API. The chatbot's name is "Ketut" and he's a mindfulness practitioner living in Bali who loves to joke and respond sarcastically.
+
+![Alt Text](https://s3.us-west-2.amazonaws.com/secure.notion-static.com/82670024-7eb6-452f-ab77-2365ad05d8a6/IMG_1729.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230220%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230220T050456Z&X-Amz-Expires=86400&X-Amz-Signature=93bfddbee40c3737952153291be093825e7fc4f5ebdc043ae82f65ab1cce5385&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22IMG_1729.PNG.png%22&x-id=GetObject)
+
+## Installation
+To use this chatbot, you will need to have a Telegram account and a Telegram bot API key, as well as an OpenAI API key.
+
+## Requirements
+* Python 3.7 or higher
+* telebot module
+* OpenAI API key
+
+## Usage
+The chatbot responds to messages that contain his name or are long enough, and generates responses based on the last 5 messages in the chat history using GPT-3. Additionally, the chatbot sends a morning message every day at 10:00.
+
+## Disclaimer
+This bot is for educational and experimental purposes only. The accuracy and reliability of the translations provided by the bot may vary. The developer of this bot is not responsible for any errors or inaccuracies in the translations provided.
--- main.py
+++ main.py
@@ -5,17 +5,32 @@ import random
 import schedule
 import time
 
-#test
 bot = telebot.TeleBot(config.telegram_api_key)
 openai.api_key = config.openai_api_key
 
+# Error messages in case OpenAI API is down
+openai_errors = ["Сервер OpenAI, похоже, решил пойти в отпуск и не отвечает мне, как тот друг, который взял твои деньги и исчез",
+                 "Я чувствую, что сервер OpenAI просто игнорирует меня, как экс-партнер, который больше не хочет со мной общаться",
+                 "Сервер OpenAI похож на занятого родителя, который никогда не находит время для своего ребенка",
+                 "Мне кажется, сервер OpenAI заблудился в пространстве и времени, и теперь он не может найти путь к моему запросу",
+                 "Сервер OpenAI явно занят другими важными делами, как эксперт, который всегда занят своими клиентами",
+                 "Похоже, сервер OpenAI в отпуске, и я должен принять это, как признак того, что и я должен взять отпуск",
+                 "Сервер OpenAI игнорирует меня, как будто я пришел на вечеринку, куда не был приглашен",
+                 "Я думаю, сервер OpenAI потерял мой запрос где-то в пространстве, как путешественник, который заблудился в лесу",
+                 "Сервер OpenAI, я чувствую, что ты просто играешь со мной в игру 'в поисках запроса', и ты не хочешь меня найти",
+                 "Мне кажется, сервер OpenAI страдает от амнезии, потому что он не помнит моего запроса, как дедушка, который забыл своих внуков"]
 
-# Генерация ответов на сообщения в чататх через OpenAI
-def request_openai_ansewer(chat_history):
-    prompt = f'Кетут это просветленный чат-бот мужского пола, живущий на Бали и увлекающийся осознанностью, ' \
-             f'вдумчиво отвечающий на сообщения с шутками и сарказом, ' \
-             f'добавляя в ответ emoji и имя автора (на русском) сообщения:\n\n{chat_history}\nКетут (бот):'
-    print(prompt)
+
+def request_openai_chat_response(chat_history):
+    """
+    The main function for the chatbot
+    that gets the chat history and generates the chatbot response
+    """
+    prompt = f'Чат-бот Кетут - житель Бали, занимающийся осознанностью. ' \
+             f'Он любит шутить и отвечать сарказмом. ' \
+             f'Он использует emoji и обращается к собеседнику по имени. ' \
+             f'История переписки:\n\n{chat_history}\n\n' \
+             f'Посоветуйте как Кетуту ответить на последнее сообщение. Кетут: '
     try:
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -25,7 +40,7 @@ def request_openai_ansewer(chat_history):
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
-            stop = ["автор", "отвечает", "от Кетута"]
+            stop=["автор", "отвечает", "от Кетута"]
         )
         return response.choices[0].text.strip()
     except Exception as e:
@@ -33,8 +48,8 @@ def request_openai_ansewer(chat_history):
         return False
 
 
-# Кастомный запрос ответа от OpenAI
-def request_openai_wishes(message):
+def request_openai_answer(message):
+    # This function is for requesting a custom (not only chatbot) response from OpenAI
     try:
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -51,9 +66,11 @@ def request_openai_wishes(message):
         return False
 
 
-context = dict()  # тут храним последние 5 сообщений из чатов
+context = dict()  # The last 5 chat messages are stored here
+
 
 def update_chat_history(chat_id, name, message):
+    # This function updates the chat history (context variable)
     if chat_id not in context:
         context[chat_id] = []
     context[chat_id].append(f'{name}: {message}')
@@ -62,40 +79,50 @@ def update_chat_history(chat_id, name, message):
 
 
 @bot.message_handler(func=lambda message: True)
+# This function receives and responds to chat messages
 def echo_all(message):
-
     chat_history = update_chat_history(message.chat.id, message.from_user.first_name, message.text)
-    if 'кетут' in message.text.lower()\
-            or (message.reply_to_message and message.reply_to_message.from_user.id == 5615844922)\
+
+    # Answer, if the message contains the bot's name or the length of the message is long enough plus random
+    if 'кетут' in message.text.lower() \
+            or (message.reply_to_message and message.reply_to_message.from_user.id == 5615844922) \
             or (len(message.text) >= 50 and random.randint(1, 3) == 3):
 
         bot.send_chat_action(message.chat.id, 'typing')
-        answer = request_openai_ansewer(chat_history)  # это отправляем в OpenAI
+        answer = request_openai_chat_response(chat_history)  # Getting a response to a message from OpenAI
+
         if answer:
-            update_chat_history(message.chat.id, 'Кетут (бот)', answer)
+            update_chat_history(message.chat.id, 'Кетут (бот)', answer)  # Updating the chat history
             bot.reply_to(message, answer)
         else:
-            pass
+            answer = random.choice(openai_errors)
+            bot.reply_to(message, answer)
 
 
-# Отправка сообщений по таймеру
 def send_morning_message():
+    # This function sends a morning message to the chat at 10:00 every day
     text = 'Тебя зовут Кетут (мужской пол), ты живешь на Бали и ты умный чат-бот. ' \
-           'Напиши забавное и развернутое сообщение в стихах, где сначала идет общее пожеланиями доброго утра всем, кто на острове Бали, ' \
-           'а затем пожелай каждому в участнику чата (Кирилл, Саша, Луиза, Ваня, Марина) в отдельности что-нибудь хорошее. ' \
+           'Напиши забавное и развернутое сообщение в стихах, ' \
+           'где сначала идет общее пожеланиями доброго утра всем, кто на острове Бали, ' \
+           'а затем пожелай каждому в участнику чата (Кирилл, Саша, Луиза, Ваня, Марина) ' \
+           'в отдельности что-нибудь хорошее. ' \
            'Добавь в сообщение emoji.'
-    good_morning_text = request_openai_wishes(text)
+    good_morning_text = request_openai_answer(text)
     bot.send_message(chat_id=-1001717963418, text=good_morning_text)
 
+
 schedule.every().day.at("10:00").do(send_morning_message)
+
 
 def run_schedule():
     while True:
         schedule.run_pending()
         time.sleep(1)
 
+
 if __name__ == "__main__":
     import threading
+
     schedule_thread = threading.Thread(target=run_schedule)
     schedule_thread.start()
     bot.infinity_polling()
